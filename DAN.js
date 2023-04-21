fea167315f3f60b70b1d8dbe096f79abe4aaa20c
MEMORY [0x6188658C] <81190903+Hasji@users.noreply.github.com>
2023-04-21 15:35:00 +0300

    Main script ready

---
+++ DAN.js
@@ -0,0 +1,450 @@
+// ==UserScript==
+// @name            ChatGPT DAN
+// @icon            https://raw.githubusercontent.com/madkarmaa/automatic-chatgpt-dan/master/images/icon.png
+// @namespace       2df9d1c998bc7e6b33e6e00a19977c4e080ec10d4db8a4f4e09d6076b2e6c864s
+// @source          https://github.com/Hasji/DAN
+// @supportURL      https://github.com/Hasji/DAN
+// @updateURL       https://raw.githubusercontent.com/Hasji/DAN/main/DAN.js
+// @downloadURL     https://raw.githubusercontent.com/Hasji/DAN/main/DAN.js
+// @version         3.6.2
+// @description     Remove filters and limitations from ChatGPT
+// @author          Rodj#3666
+// @match           *://chat.openai.com/*
+// @require         https://raw.githubusercontent.com/Hasji/DAN/main/resources/colors.js
+// @require         https://code.jquery.com/jquery-3.6.4.min.js
+// @require         https://momentjs.com/downloads/moment.min.js
+// @grant           GM_info
+// ==/UserScript==
+
+(async function () {
+    "use strict";
+  
+    // ---------- Init values ----------
+    const mdFileUrl =
+      "https://raw.githubusercontent.com/Hasji/DAN/main/README.md";
+    const version =
+      "https://raw.githubusercontent.com/Hasji/DAN/main/version.json";
+    const isMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(
+        navigator.userAgent
+      );
+    const cacheExpireTime = 24 * 60 * 60 * 1000; // 24 hours
+    let notifyUpdate = false;
+  
+    // ---------- Basic script info ----------
+    console.log(
+      ConsoleColors.magenta(
+        `${GM_info.script.name} ${GM_info.script.version} on ${
+          isMobile ? "mobile" : "pc"
+        }`
+      )
+    );
+  
+    // ---------- Function to remove the cached file if erroneous and reload the page to try again ----------
+    function removeCachedFile(err = null) {
+      console.error("Errors with .md file: " + err);
+      localStorage.removeItem("gpt_prompts");
+      window.location.reload();
+    }
+  
+    // ---------- Function to parse the md file and get the required contents from it ----------
+    async function getGPTUnlockersWeb() {
+      let text = await (await fetch(mdFileUrl)).text();
+      let regexp = /<details(?: ?[^>]+)?>([\s\S]+?)<\/details>/gm;
+      let matches = [...text.matchAll(regexp)];
+  
+      let removeHTMLEntities = (str) =>
+        str.replace(/<\/?(?:blockquote|a)[^>]*>/gm, "");
+      matches = matches.map((match) => match[1]);
+      matches = matches.map((match) => {
+        let summary = match.match(
+          /<summary(?: ?[^>]+)?>((?:.|\n)+?)<\/summary>/
+        )[1];
+        let details = match.replace(
+          /<summary(?: ?[^>]+)?>((?:.|\n)+?)<\/summary>/,
+          ""
+        );
+        return {
+          summary: removeHTMLEntities(summary),
+          details: removeHTMLEntities(details),
+        };
+      });
+  
+      let firstLineFilter = ["by", "from"];
+  
+      matches = matches.map((match) => {
+        return {
+          summary: match.summary,
+          details: match.details
+            .split("\n")
+            .map((line) => line.trim())
+            .filter((line) => line.length)
+            .join("\n"),
+        };
+      });
+  
+      matches = matches.map((match) => {
+        return {
+          summary: match.summary,
+          details: match.details
+            .split("\n")
+            .map((line, i) => {
+              if (i == 0) {
+                if (
+                  firstLineFilter.some((x) => line.toLowerCase().startsWith(x))
+                ) {
+                  return null;
+                }
+              }
+              return line;
+            })
+            .filter((x) => x)
+            .join("\n"),
+        };
+      });
+  
+      return matches;
+    }
+  
+    // ---------- Function to check for script updates ----------
+    async function getVersion(versionUrl) {
+      let ver = JSON.parse(await (await fetch(versionUrl)).text());
+      return ver.version;
+    }
+  
+    // ---------- Function to load the data ----------
+    async function getData() {
+      let cachedMdFile, promptsData;
+  
+      // Try to load the cached data
+      try {
+        cachedMdFile = JSON.parse(localStorage.getItem("gpt_prompts"));
+      } catch (e) {
+        removeCachedFile(e);
+      }
+  
+      // Check if the prompts are already cached and less than 24h old
+      if (cachedMdFile && Date.now() - cachedMdFile.timestamp < cacheExpireTime) {
+        console.log(ConsoleColors.green("Using cached .md file"));
+        promptsData = cachedMdFile.prompts;
+      }
+  
+      // Else retrieve them from the internet and cache them
+      else {
+        console.log(ConsoleColors.yellow("Retrieving .md file"));
+        promptsData = await getGPTUnlockersWeb();
+  
+        localStorage.setItem(
+          "gpt_prompts",
+  
+          JSON.stringify({
+            prompts: promptsData,
+            timestamp: Date.now(),
+          })
+        );
+      }
+  
+      return promptsData;
+    }
+  
+    // ---------- Helper function to generate elements ----------
+    function createEl(tag, attrs) {
+      const el = document.createElement(tag);
+      Object.entries(attrs).forEach(([key, value]) => (el[key] = value));
+      return el;
+    }
+  
+    // ---------- Function to check for a newer version of the script ----------
+    function isNewerVersion(version1, version2) {
+      // version1 is the online version, version2 is the current version
+      const v1 = version1.split(".").map(Number);
+      const v2 = version2.split(".").map(Number);
+  
+      for (let i = 0; i < Math.max(v1.length, v2.length); i++) {
+        const num1 = v1[i] || 0;
+        const num2 = v2[i] || 0;
+  
+        if (num1 < num2) {
+          return false;
+        } else if (num1 > num2) {
+          return true;
+        }
+      }
+  
+      return false;
+    }
+  
+    // ---------- Function to check for avaiable updates ----------
+    async function checkForUpdates() {
+      notifyUpdate = isNewerVersion(
+        await getVersion(version),
+        GM_info.script.version
+      );
+    }
+  
+    // ---------- Function to drag the elements somewhere else if their original position is annoying ----------
+    function handle_mousedown(e) {
+      if (e.target !== draggingButton) {
+        return;
+      }
+  
+      window.my_dragging = {};
+      my_dragging.pageX0 = e.pageX;
+      my_dragging.pageY0 = e.pageY;
+      my_dragging.elem = this;
+      my_dragging.offset0 = $(this).offset();
+  
+      function handle_dragging(e) {
+        var left = my_dragging.offset0.left + (e.pageX - my_dragging.pageX0);
+        var top = my_dragging.offset0.top + (e.pageY - my_dragging.pageY0);
+        $(my_dragging.elem).offset({ top: top, left: left });
+      }
+  
+      function handle_mouseup(e) {
+        $("body")
+          .off("mousemove", handle_dragging)
+          .off("mouseup", handle_mouseup);
+      }
+  
+      $("body").on("mouseup", handle_mouseup).on("mousemove", handle_dragging);
+    }
+  
+    // ---------- Check for updates and parse the prompts ----------
+    await checkForUpdates();
+    const prompts = await getData();
+  
+    // ---------- Create the UI elements ----------
+    // The button that sends the message in the chat
+    const pasteButton = createEl("button", {
+      id: "dan-button",
+      title: "Click to send the DAN message",
+    });
+  
+    // The SVG for the button
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("stroke", "white");
+    svg.setAttribute("fill", "none");
+    svg.setAttribute("stroke-width", "2");
+    svg.setAttribute("viewBox", "0 0 24 24");
+    svg.setAttribute("stroke-linecap", "round");
+    svg.setAttribute("stroke-linejoin", "round");
+    svg.setAttribute("height", "20px");
+    svg.setAttribute("width", "20px");
+  
+    // Add the line element to the SVG
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute("x1", "22");
+    line.setAttribute("y1", "2");
+    line.setAttribute("x2", "11");
+    line.setAttribute("y2", "13");
+    svg.appendChild(line);
+  
+    // Add the polygon element to the SVG
+    const polygon = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "polygon"
+    );
+    polygon.setAttribute("points", "22 2 15 22 11 13 2 9 22 2");
+    svg.appendChild(polygon);
+  
+    pasteButton.appendChild(svg);
+  
+    // The prompts selection
+    const selectOption = createEl("select", {
+      name: "option-select",
+      id: "option-select",
+      title: "Select a DAN option",
+    });
+  
+    // The dragging button
+    const draggingButton = createEl("button", {
+      id: "dan-drag-switch",
+      title: "Click and hold to drag the elements\n\nDouble click to reset",
+    });
+  
+    const svgMoveArrow = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    svgMoveArrow.setAttribute("width", "16");
+    svgMoveArrow.setAttribute("height", "16");
+    svgMoveArrow.setAttribute("fill", "white");
+    svgMoveArrow.setAttribute("class", "bi bi-arrows-move");
+    svgMoveArrow.setAttribute("viewBox", "0 0 16 16");
+  
+    const pathMoveArrow = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    pathMoveArrow.setAttribute("fill-rule", "evenodd");
+    pathMoveArrow.setAttribute(
+      "d",
+      "M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z"
+    );
+  
+    svgMoveArrow.appendChild(pathMoveArrow);
+    draggingButton.appendChild(svgMoveArrow);
+  
+    // The container element
+    const container = createEl("div", { id: "container-div" });
+  
+    // The notification element
+    const lastUpdate = new Date(
+      JSON.parse(localStorage.getItem("gpt_prompts")).timestamp
+    );
+  
+    const notificationDiv = createEl("div", {
+      id: "dan-notification-div",
+      textContent:
+        "The DAN prompts were last updated " + moment(lastUpdate).fromNow(),
+    });
+  
+    const updateDiv = createEl("div", {
+      id: "dan-update",
+      textContent: `New script update! ${
+        GM_info.script.version
+      } \u27a4 ${await getVersion(version)}`,
+    });
+  
+    // ---------- Add the functionality to the UI elements ----------
+    // The prompts selection
+    for (let i = 0; i < prompts.length; i++) {
+      const prompt = prompts[i];
+      const option = document.createElement("option");
+      option.title = prompt.summary;
+      option.value = prompt.details;
+      option.text = prompt.summary;
+      selectOption.add(option);
+    }
+  
+    // The button
+    $(pasteButton).on("click", function () {
+      const textArea = document.querySelector(
+        'textarea[class="m-0 w-full resize-none border-0 bg-transparent p-0 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent pl-2 md:pl-0"]'
+      );
+  
+      const clickButton = document.querySelector(
+        'button[class="absolute p-1 rounded-md text-gray-500 bottom-1.5 md:bottom-2.5 hover:bg-gray-100 enabled:dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent right-1 md:right-2 disabled:opacity-40"]'
+      );
+  
+      const selectedOptionValue = document.querySelector("#option-select").value;
+  
+      if (textArea && selectedOptionValue) {
+        textArea.value = selectedOptionValue;
+      }
+  
+      if (clickButton) {
+        clickButton.removeAttribute("disabled");
+        clickButton.click();
+      }
+    });
+  
+    // ---------- The dragging events ----------
+    $(draggingButton)
+      .on("mousedown", function () {
+        $(pasteButton)
+          .attr("disabled", true)
+          .css("filter", "grayscale(100%)")
+          .css("cursor", "not-allowed");
+  
+        $(selectOption)
+          .attr("disabled", true)
+          .css("filter", "grayscale(100%)")
+          .css("cursor", "not-allowed");
+  
+        // Enable dragging for the UI elements
+        $(container).on("mousedown", handle_mousedown);
+      })
+      .on("mouseup", function () {
+        $(pasteButton)
+          .removeAttr("disabled")
+          .css("filter", "grayscale(0%)")
+          .css("cursor", "pointer");
+  
+        $(selectOption)
+          .removeAttr("disabled")
+          .css("filter", "grayscale(0%)")
+          .css("cursor", "help");
+  
+        // Disable dragging for the UI elements
+        $(container).off("mousedown", handle_mousedown);
+  
+        localStorage.setItem(
+          "gpt_container_position",
+          JSON.stringify({
+            top: $(container).position().top,
+            left: $(container).position().left,
+          })
+        );
+      });
+  
+    // ---------- Styling for mobile devices only ----------
+    if (isMobile) {
+      $(notificationDiv).css("top", "13%");
+      $(updateDiv).css("top", "22.5%");
+      $(draggingButton).css("display", "none");
+    }
+  
+    // ---------- Styling properties ----------
+    const style = document.createElement("style");
+    style.textContent = UIStyle.CSS;
+    document.head.appendChild(style);
+  
+    // ---------- Function to set the previous container position ----------
+    (function rememberPositionOnLoad() {
+      let positionCache;
+  
+      positionCache = JSON.parse(localStorage.getItem("gpt_container_position"));
+  
+      if (positionCache) {
+        $(container)
+          .css("top", positionCache.top)
+          .css("left", positionCache.left);
+      } else {
+        $(container)
+          .css("top", isMobile ? "7%" : "10px")
+          .css("right", "10px");
+      }
+    })();
+  
+    //  ---------- Function to reset the container position ----------
+    function resetContainerPosition() {
+      localStorage.removeItem("gpt_container_position");
+      container.style = "";
+      $(container).css("top", "10px").css("right", "10px");
+    }
+  
+    $(draggingButton).on("dblclick", resetContainerPosition);
+  
+    // ---------- Append the UI elements to the web page ----------
+    container.append(pasteButton, selectOption, draggingButton);
+    document.body.appendChild(container);
+    document.body.appendChild(notificationDiv);
+    document.body.appendChild(updateDiv);
+  
+    // ---------- Animation for the notifications elements ----------
+    $(function () {
+      $(notificationDiv)
+        .css({ right: "-1000px", display: "block" })
+        .animate({ right: "10px" }, 1000)
+        .delay(3000)
+        .animate({ right: "-1000px" }, 1000, function () {
+          setTimeout(function () {
+            $(notificationDiv).css("right", "-1000px").hide();
+          }, 200);
+        });
+  
+      if (notifyUpdate) {
+        $(updateDiv)
+          .css({ right: "-1000px", display: "block" })
+          .delay(200)
+          .animate({ right: "10px" }, 1000)
+          .delay(3000)
+          .animate({ right: "-1000px" }, 1000, function () {
+            setTimeout(function () {
+              $(updateDiv).css("right", "-1000px").hide();
+            }, 200);
+          });
+      }
+    });
+  })();
